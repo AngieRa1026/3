8087f64d207fe3e7d2b891c29f8d1946d4a28bd7
Angie Ramirez <angieramirez1026@gmail.com>
2025-02-17 22:44:15 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
-
 public class main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("ingrese un numero: ");
-        int numero = scanner.nextInt();
-        int resultado = numero + 10;
-        System.out.println("el resultado es: " + resultado);
+        // declaracion de variables con buenas practicas de nombrados
+        int studentCount = 25;
+        double averageGrado = 87.5;
+        String courseName = "introduccion to java";
+        boolean isCourseActive = true;
+
+        // imprimir los valores de las variables
+            System.out.println("Course Name: " + courseName);
+            System.out.println("Number of students: " + studentCount);
+            System.out.println("Average grade: " + averageGrado);
+            System.out.println("Course active: " + isCourseActive);
+
     }
 }
