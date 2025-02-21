a0687ebdf36312f932da890ca883d36a2af7dba8
Angie Ramirez <angieramirez1026@gmail.com>
2025-02-21 11:11:35 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,15 @@
 public class main {
     public static void main(String[] args) {
         // declaracion de variables con buenas practicas de nombrados
-        int studentCount = 25;
-        double averageGrado = 87.5;
+        byte numStudentCount = 25;
+        float numAverageGrado = 87.5f;
         String courseName = "introduccion to java";
         boolean isCourseActive = true;
 
         // imprimir los valores de las variables
             System.out.println("Course Name: " + courseName);
-            System.out.println("Number of students: " + studentCount);
-            System.out.println("Average grade: " + averageGrado);
+            System.out.println("Number of students: " + numStudentCount);
+            System.out.println("Average grade: " + numAverageGrado);
             System.out.println("Course active: " + isCourseActive);
 
     }
